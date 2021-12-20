d85233e215f1248fef81774a495b58bf1e65b691
AyannaRenee <Hawkins31.ayanna@gmail.com>
2021-12-19 20:39:03 -0600

    update 3

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const img = document.querySelector('img')
+
+const btn1 = document.querySelector('#btn1')
+const btn2 = document.querySelector('#btn2')
+const btn3 = document.querySelector('#btn3')
+const btn4 = document.querySelector('#btn4')
+const btn5 = document.querySelector('#btn5')
+const btn6 = document.querySelector('#btn6')
+
+btn1.addEventListener('click', () => {
+  img.src = 'caramel.png'
+})
